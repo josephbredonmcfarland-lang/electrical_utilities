59a34de6725e35ac42db943e29649e943c65851c
josephbredonmcfarland-lang <josephbredonmcfarland@gmail.com>
2026-08-04 15:12:02 -0400

    Combinations

--- cpp/Math/ReciprocalSum.cpp
+++ cpp/Math/ReciprocalSum.cpp
@@ -4,6 +4,9 @@
 double ReciprocalSum(std::initializer_list<double> list) {
     double num = 0;
     for (const double& x : list) {
+        if (x == 0) {
+            throw std::invalid_argument("List value cannot be 0.");
+        }
         num += 1/x;
     }
     return std::pow(num, -1);
--- cpp/Math/ReciprocalSum.h
+++ cpp/Math/ReciprocalSum.h
@@ -5,6 +5,7 @@
 #ifndef ELECTRICAL_UTILITIES_RECIPROCALSUM_H
 #define ELECTRICAL_UTILITIES_RECIPROCALSUM_H
 #include <vector>
+#include <stdexcept>
 
 
 double ReciprocalSum(std::initializer_list<double> list);
