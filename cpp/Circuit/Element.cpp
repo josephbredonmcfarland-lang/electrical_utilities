1cab745262ac47c5fe12a3907227fd63653dfc06
josephbredonmcfarland-lang <josephbredonmcfarland@gmail.com>
2026-08-06 00:14:58 -0400

    nodes&branches update

--- cpp/Circuit/Element.cpp
+++ cpp/Circuit/Element.cpp
@@ -1,1 +1,21 @@
 #include "Element.h"
+#include <string>
+#include <stdexcept>
+
+// Constructor
+Element::Element(std::string id, double value)
+    : id(id), value(value), unit("") {
+    validate();
+};
+// Validate input value
+void Element::validate() {
+    if (value <= 0) {
+        throw std::invalid_argument("Value must be positive");
+    }
+}
+
+Resistor::Resistor(std::string id, double value)
+    : Element(id, value, unit("Ω")) {
+
+};
+
--- cpp/Circuit/Element.h
+++ cpp/Circuit/Element.h
@@ -1,10 +1,28 @@
 #ifndef ELECTRICAL_UTILITIES_ELEMENT_H
 #define ELECTRICAL_UTILITIES_ELEMENT_H
-
+#include <string>
 
 class Element {
-
+public:
+    std::string id;
+    double value;
+    std::string unit;
+    Element(std::string id, double value);
+    void validate();
 };
+
+class Resistor: public Element {
+    Resistor(std::string id, double value);
+};
+
+class Capacitor: public Element {
+    Capacitor(std::string id, double value);
+};
+
+class Inductor: public Element {
+    Inductor(std::string id, double value);
+};
+
 
 
 #endif //ELECTRICAL_UTILITIES_ELEMENT_H
